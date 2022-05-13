5dc7288b1c97a33368a95cc0f72b209ab5826ba1
Jacquin-noe <105502405+Jacquin-noe@users.noreply.github.com>
2022-05-13 16:40:49 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function foncEntre(){
-	document.querySelector("#maDiv").classList.remove("blanc");
-	document.querySelector("#maDiv").classList.add("rouge");
-}
-function foncQuitte() {
-	document.querySelector("#maDiv").classList.remove("rouge");
-	document.querySelector("#maDiv").classList.add("blanc");
-}
